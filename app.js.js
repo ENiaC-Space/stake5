f0a2842814d03f30469157520466b126b131d32f
Enaa <eniacspace@gmail.com>
2025-12-25 15:48:39 +0700

    app.js

--- app.js.js
+++ app.js.js
@@ -1,40 +1,137 @@
 let web3, account;
 
-// ===== MAINNET ADDRESS =====
-const ENIAC = "0xafF339de48848d0F8B5704909Ac94e8E8D7E3415";
+// ===== MAINNET ADDRESS (THEO FILE ABI ANH GỬI) =====
 const MASTERCHEF = "0x564DF71B75855d63c86a267206Cd0c9e35c92789";
-const PID_STAKE = 0;
-const CHAIN_ID = "0x38"; // BSC (đổi nếu chain khác)
+const CHAIN_ID = "0x38"; // BSC mainnet
 
-// ===== ABI RÚT GỌN =====
-const ERC20_ABI = [
-  {"inputs":[{"name":"spender","type":"address"},{"name":"amount","type":"uint256"}],"name":"approve","type":"function"},
-  {"inputs":[{"name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"type":"uint256"}],"type":"function"}
+// ===== ABI ĐẦY ĐỦ – ĐÚNG FILE ANH GỬI =====
+const MASTER_ABI = [
+  {
+    "inputs":[
+      {"internalType":"contract IANT","name":"_ANT","type":"address"},
+      {"internalType":"contract IVault","name":"_vault","type":"address"},
+      {"internalType":"address","name":"_devaddr","type":"address"},
+      {"internalType":"uint256","name":"_ANTPerBlock","type":"uint256"},
+      {"internalType":"uint256","name":"_startBlock","type":"uint256"}
+    ],
+    "type":"constructor"
+  },
+  {
+    "inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],
+    "name":"updatePool",
+    "outputs":[],
+    "stateMutability":"nonpayable",
+    "type":"function"
+  },
+  {
+    "inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],
+    "name":"poolInfo",
+    "outputs":[
+      {"internalType":"contract IERC20","name":"lpToken","type":"address"},
+      {"internalType":"uint256","name":"allocPoint","type":"uint256"},
+      {"internalType":"uint256","name":"lastRewardBlock","type":"uint256"},
+      {"internalType":"uint256","name":"accANTPerShare","type":"uint256"}
+    ],
+    "stateMutability":"view",
+    "type":"function"
+  },
+  {
+    "inputs":[
+      {"internalType":"uint256","name":"_pid","type":"uint256"},
+      {"internalType":"address","name":"_user","type":"address"}
+    ],
+    "name":"userInfo",
+    "outputs":[
+      {"internalType":"uint256","name":"amount","type":"uint256"},
+      {"internalType":"uint256","name":"rewardDebt","type":"uint256"}
+    ],
+    "stateMutability":"view",
+    "type":"function"
+  },
+  {
+    "inputs":[
+      {"internalType":"uint256","name":"_pid","type":"uint256"},
+      {"internalType":"uint256","name":"_amount","type":"uint256"}
+    ],
+    "name":"deposit",
+    "outputs":[],
+    "stateMutability":"nonpayable",
+    "type":"function"
+  },
+  {
+    "inputs":[
+      {"internalType":"uint256","name":"_pid","type":"uint256"},
+      {"internalType":"uint256","name":"_amount","type":"uint256"}
+    ],
+    "name":"withdraw",
+    "outputs":[],
+    "stateMutability":"nonpayable",
+    "type":"function"
+  },
+  {
+    "inputs":[
+      {"internalType":"uint256","name":"_pid","type":"uint256"},
+      {"internalType":"address","name":"_user","type":"address"}
+    ],
+    "name":"pendingANT",
+    "outputs":[{"internalType":"uint256","name":"","type":"uint256"}],
+    "stateMutability":"view",
+    "type":"function"
+  },
+  {
+    "inputs":[],
+    "name":"poolLength",
+    "outputs":[{"internalType":"uint256","name":"","type":"uint256"}],
+    "stateMutability":"view",
+    "type":"function"
+  }
 ];
 
-const MASTER_ABI = [
-  {"inputs":[{"name":"_pid","type":"uint256"},{"name":"_amount","type":"uint256"}],"name":"deposit","type":"function"},
-  {"inputs":[{"name":"_pid","type":"uint256"},{"name":"_amount","type":"uint256"}],"name":"withdraw","type":"function"},
-  {"inputs":[{"name":"_pid","type":"uint256"},{"name":"_user","type":"address"}],"name":"pendingANT","outputs":[{"type":"uint256"}],"type":"function"},
-  {"inputs":[],"name":"poolLength","outputs":[{"type":"uint256"}],"type":"function"},
-  {"inputs":[{"type":"uint256"}],"name":"poolInfo","outputs":[{"type":"address"},{"type":"uint256"},{"type":"uint256"},{"type":"uint256"}],"type":"function"},
-  {"inputs":[{"type":"uint256"},{"type":"address"}],"name":"userInfo","outputs":[{"type":"uint256"},{"type":"uint256"}],"type":"function"},
-  {"inputs":[],"name":"ANTPerBlock","outputs":[{"type":"uint256"}],"type":"function"}
+// ===== ERC20 ABI ĐẦY ĐỦ =====
+const ERC20_ABI = [
+  {
+    "constant":false,
+    "inputs":[
+      {"name":"spender","type":"address"},
+      {"name":"amount","type":"uint256"}
+    ],
+    "name":"approve",
+    "outputs":[{"name":"","type":"bool"}],
+    "type":"function"
+  },
+  {
+    "constant":true,
+    "inputs":[{"name":"owner","type":"address"}],
+    "name":"balanceOf",
+    "outputs":[{"name":"","type":"uint256"}],
+    "type":"function"
+  },
+  {
+    "constant":true,
+    "inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],
+    "name":"allowance",
+    "outputs":[{"name":"","type":"uint256"}],
+    "type":"function"
+  }
 ];
 
 // ===== INIT =====
 document.getElementById("connectBtn").onclick = connect;
 
-function token(){ return new web3.eth.Contract(ERC20_ABI, ENIAC); }
-function master(){ return new web3.eth.Contract(MASTER_ABI, MASTERCHEF); }
+function master(){
+  return new web3.eth.Contract(MASTER_ABI, MASTERCHEF);
+}
 
-// ===== CONNECT =====
+// ===== CONNECT WALLET =====
 async function connect(){
-  if(!window.ethereum) return alert("Install MetaMask");
+  if(!window.ethereum){
+    alert("Cài MetaMask trước");
+    return;
+  }
 
-  if (ethereum.chainId !== CHAIN_ID) {
+  if (ethereum.chainId !== CHAIN_ID){
     await ethereum.request({
-      method: 'wallet_switchEthereumChain',
+      method: "wallet_switchEthereumChain",
       params: [{ chainId: CHAIN_ID }]
     });
   }
@@ -46,87 +143,53 @@ async function connect(){
   document.getElementById("wallet").innerText =
     account.slice(0,6) + "..." + account.slice(-4);
 
-  loadStake();
-  loadFarms();
+  loadPools();
 }
 
-// ===== STAKE =====
-async function approve(){
-  await token().methods
-    .approve(MASTERCHEF, web3.utils.toWei("100000000"))
-    .send({from: account});
-}
-
-async function stake(){
-  const v = document.getElementById("amount").value;
-  if(v <= 0) return;
-  await master().methods.deposit(PID_STAKE, web3.utils.toWei(v))
-    .send({from: account});
-  loadStake();
-}
-
-async function withdraw(){
-  const v = document.getElementById("amount").value;
-  await master().methods.withdraw(PID_STAKE, web3.utils.toWei(v))
-    .send({from: account});
-  loadStake();
-}
-
-async function harvest(){
-  await master().methods.deposit(PID_STAKE, 0)
-    .send({from: account});
-  loadStake();
-}
-
-async function loadStake(){
-  const user = await master().methods.userInfo(PID_STAKE, account).call();
-  const pending = await master().methods.pendingANT(PID_STAKE, account).call();
-
-  document.getElementById("staked").innerText =
-    web3.utils.fromWei(user.amount);
-  document.getElementById("pending").innerText =
-    web3.utils.fromWei(pending);
-
-  // APR (ước tính)
-  const antPB = await master().methods.ANTPerBlock().call();
-  const tvl = await token().methods.balanceOf(MASTERCHEF).call();
-  const apr = tvl > 0 ? (antPB * 10512000 / tvl) * 100 : 0;
-  document.getElementById("apr").innerText = apr.toFixed(2) + "%";
-}
-
-// ===== FARM MULTI POOL =====
-async function loadFarms(){
+// ===== LOAD POOLS (QUAN TRỌNG) =====
+async function loadPools(){
   const len = await master().methods.poolLength().call();
   const div = document.getElementById("farms");
   div.innerHTML = "";
 
   for(let pid=0; pid<len; pid++){
+    const pool = await master().methods.poolInfo(pid).call();
     div.innerHTML += `
       <div class="farm">
         <b>Pool #${pid}</b><br>
-        <input id="f${pid}" placeholder="Amount">
-        <button onclick="depositFarm(${pid})">Stake</button>
-        <button onclick="withdrawFarm(${pid})">Unstake</button>
+        <small>Stake token: ${pool.lpToken}</small><br>
+        <input id="amt${pid}" placeholder="Amount">
+        <button onclick="approveToken(${pid})">Approve</button>
+        <button onclick="stake(${pid})">Stake</button>
+        <button onclick="withdraw(${pid})">Withdraw</button>
       </div>
     `;
   }
 }
 
-async function depositFarm(pid){
-  const v = document.getElementById("f"+pid).value;
-  await master().methods.deposit(pid, web3.utils.toWei(v))
-    .send({from: account});
+// ===== APPROVE ĐÚNG TOKEN CỦA POOL =====
+async function approveToken(pid){
+  const pool = await master().methods.poolInfo(pid).call();
+  const token = new web3.eth.Contract(ERC20_ABI, pool.lpToken);
+
+  await token.methods
+    .approve(MASTERCHEF, web3.utils.toWei("1000000000"))
+    .send({ from: account });
 }
 
-async function withdrawFarm(pid){
-  const v = document.getElementById("f"+pid).value;
-  await master().methods.withdraw(pid, web3.utils.toWei(v))
-    .send({from: account});
+// ===== STAKE / WITHDRAW =====
+async function stake(pid){
+  const val = document.getElementById("amt"+pid).value;
+  if(!val || val <= 0) return;
+
+  await master().methods
+    .deposit(pid, web3.utils.toWei(val))
+    .send({ from: account });
 }
 
-// ===== UI =====
-function showTab(id){
-  document.getElementById("stake").style.display="none";
-  document.getElementById("farm").style.display="none";
-  document.getElementById(id).style.display="block";
+async function withdraw(pid){
+  const val = document.getElementById("amt"+pid).value;
+  await master().methods
+    .withdraw(pid, web3.utils.toWei(val))
+    .send({ from: account });
 }
